1c207492c25e39bd9beb7a2a87f80173f0ef1e21
naumanjadev <156223285+Hania-Khan@users.noreply.github.com>
2024-07-03 02:04:33 +0500

    added a file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,13 +4,13 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiUFUl-50t2rXrOSSff491oRwfHfVyk74",
-  authDomain: "healthcane.firebaseapp.com",
-  projectId: "healthcane",
-  storageBucket: "healthcane.appspot.com",
-  messagingSenderId: "459740854347",
-  appId: "1:459740854347:web:ca338be2a5765388f3b5ce",
-  measurementId: "G-X5LBJW9SC9",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 console.log("Connected with FireBase");
 
